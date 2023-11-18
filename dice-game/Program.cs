5af2bca8cf3e64f6305f3e69f2fd0c5a1924f920
Sterre van Oploo <sterreromeijn@gmail.com>
2023-11-18 16:49:41 +0100

    Wow slay!

---
+++ dice-game/Program.cs
@@ -0,0 +1,56 @@
+﻿namespace dice_game
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("welcome to my random number generato- I mean my super fun dice game!");
+            waitForInput("> press enter to continue");
+
+            int playerOneRollResult = -1;
+            int playerTwoRollResult = -1;
+
+            Random rnd = new Random();
+
+            Console.Write("");
+            playerOneRoll();
+
+            void playerOneRoll()
+            {
+                playerOneRollResult = rnd.Next(1, 7);
+                Console.WriteLine($"your result is {playerOneRollResult}");
+                waitForInput("> ready player two");
+            }
+
+            Console.Write("");
+            playerTwoRoll();
+
+            void playerTwoRoll()    
+            {
+                playerTwoRollResult = rnd.Next(1, 7);
+                Console.WriteLine($"your result is {playerTwoRollResult}");
+                waitForInput("> show the result!");
+            }
+
+            if (playerOneRollResult > playerTwoRollResult)
+            {
+                Console.WriteLine("player one won!");
+            }
+            else if (playerOneRollResult < playerTwoRollResult)
+            {
+                Console.WriteLine("player two won!");
+            }
+            else if (playerOneRollResult == playerTwoRollResult)
+            {
+                Console.WriteLine("try again...");
+            }
+
+            void waitForInput(string message)
+            {
+                Console.WriteLine(message);
+                Console.ReadLine();
+                Console.WriteLine();
+            }
+        }
+    }
+}
